bf2cbacea21a7a4ebb6049b38185212e28dc0845
charleschu <cuxang@gmail.com>
2013-03-30 21:36:46 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LohasWorkCom::Application.config.secret_token = 'dfd99e10f9d15bba1048be239204ec2dbded9c8e25a28eeff3eccf42414c17f90fdf9d074194973ab73a865dab63890ac9b8a4ae239f85f9eaf656fab989f877'
