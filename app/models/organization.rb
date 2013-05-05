38ca4d152144c4d96849256a4ac40e63413f1929
charleschu <cuxang@gmail.com>
2013-05-05 23:39:57 +0800

    add email validate for user model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,7 @@
 class Organization < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :user
-
+  validates_presence_of :name
+  validates_uniqueness_of :name
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+# encoding: utf-8
 class User < ActiveRecord::Base
   attr_accessible :active_status, :authority, :email, :name, :online_status, :organization_id, :password, :remember_token
   belongs_to :organization
+  validates_uniqueness_of :email
+  validate :check_email
+  validates_presence_of :password
 
   def self.create_with_organization(email, pwd, organization_name)
     user = User.new(:email => email, :password => pwd)
@@ -8,6 +12,17 @@ class User < ActiveRecord::Base
     user.organization_id = organ.id
     user.save
     user
+  end
+
+  protected
+
+  def check_email
+    email_validator = /^\b[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,4}\b$/i
+    if email.blank?
+      errors[:email] = "请输入邮件地址"
+    elsif email !~ email_validator
+      errors[:email] = "邮件地址不合法"
+    end
   end
 
 end
