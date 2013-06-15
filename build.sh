432f735e5cf50d87579905f1965a697813bc90f9
san <san@lohaswork.com>
2013-06-15 22:00:14 +0800

    add check after every build step

--- build.sh
+++ build.sh
@@ -1,7 +1,31 @@
+#!/bin/bash
 bundle install
-bundle exec rake db:migrate
-bundle exec rake spec
-#reek -n app
-#tailor app/**/*.rb
-metric_fu -r
+if [ $? -ne 0 ]; then
+  echo 'bundle install error!'
+  exit 1
+fi
+echo 'bundle install success.'
 
+bundle exec rake db:migrate
+if [ $? -ne 0 ]; then
+  echo 'bundle exec rake db:migrate error!'
+  exit 1
+fi
+echo 'bundle exec rake db:migrate success.'
+
+bundle exec rake spec
+if [ $? -ne 0 ]; then
+  echo 'bundle exec rake spec error!'
+  exit 1
+fi
+echo 'bundle exec rake spec success.'
+
+#reek -n app
+tailor app/**/*.rb
+if [ $? -ne 0 ]; then
+  echo 'tailor has warning or error!'
+elif
+  echo 'tailor check has passed.'
+fi
+
+metric_fu -r
