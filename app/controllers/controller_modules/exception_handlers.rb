a601d970715ae1ef0741efa2109aa5d5972d20ae
charleschu <cuxang@gmail.com>
2013-07-07 09:45:50 +0800

    add logs for the error handle

--- app/controllers/controller_modules/exception_handlers.rb
+++ app/controllers/controller_modules/exception_handlers.rb
@@ -2,14 +2,17 @@
 require "active_record/validations.rb"
 module ControllerModules::ExceptionHandlers
   extend ActiveSupport::Concern
-  #TODO： error message mapping and logs
-
+  #TODO： error message mapping
   included do
     rescue_from ActiveRecord::RecordInvalid, :with => :handle_validation_error
   end
 
   protected
   def handle_validation_error(exception)
+    if Rails.env.development? || Rails.env.test?
+      Rails.logger.warn exception.backtrace.join("\n")
+      Rails.logger.warn exception
+    end
     error_messages = error_mapping(exception.message)
     error_messages = "unexpected error" if error_messages.blank?
     render :json => {:status => "error", :message => "Validation failed", :code => 422, :errors => error_messages }, :status => 422
