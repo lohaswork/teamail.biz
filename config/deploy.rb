fb1859b22a4e786b47652350ae8cd8a140f775a6
Cxg <serco_chen@hotmail.com>
2013-08-09 16:50:41 +0800

    fix typo in nginx.conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,8 @@ set :normalize_asset_timestamps, false
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :user, "lohaswork"
+set :server_name, "192.168.1.114"
+set :sudo_user, "lohaswork"
 set :deploy_to, "/home/lohaswork/apps/LohasWork.com"
 set :deploy_via, :remote_cache
 set :use_sudo, false
