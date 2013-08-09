1e6ec2ecca2b7567180c3eaa6202d8c55375875f
Cxg <serco_chen@hotmail.com>
2013-08-09 10:42:06 +0800

    add postgresql deployment recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,9 +7,9 @@ set :rails_env, "production"
 set :normalize_asset_timestamps, false
 #set :current_path, ""
 
-set :postgresql_user, "lohaswork"
-set :postgresql_password, Capistrano::CLI.password_prompt "PostgreSQL Password: "
-set :postgresql_database, "lohaswork_production"
+set (:postgresql_user, "lohaswork")
+set (:postgresql_password, {Capistrano::CLI.password_prompt "PostgreSQL Password: "})
+set (:postgresql_database, "lohaswork_production")
 
 namespace :postgresql do
   desc "Create a database for this application."
