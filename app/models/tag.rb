e5024840ba297a26017357746e0a33022f68a8be
Cxg <xg.chen87@gmail.com>
2013-09-17 17:15:34 +0800

    add Tag model validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,11 +1,14 @@
+# encoding: utf-8
 class Tag < ActiveRecord::Base
   attr_accessible :color, :name
 
   belongs_to :organization
+  validates :name, presence: true, :uniqueness => {:case_sensitive => true, :message => "标签名已使用"}
 
   class << self
     def create_with_organization(tag_name, organization)
       tag = Tag.new(:name => tag_name)
+      raise ValidationError.new(tag.errors.full_messages) if !tag.valid?
       organization.tags << tag
       organization.save
       tag
