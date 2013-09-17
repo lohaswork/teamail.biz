a78a9026d9d19fc7f601e4071e0391359ebfdef3
Cxg <xg.chen87@gmail.com>
2013-09-17 19:56:20 +0800

    disable tag submit when no input captured

--- app/assets/javascripts/shared/tagging_group.js
+++ app/assets/javascripts/shared/tagging_group.js
@@ -5,12 +5,20 @@
     this.init();
   }
   App.page.TaggingGroup.prototype = {
+
     init: function(){
       var self = this;
       self.el.on('click', '#tagging-dropdown', function(){
         $('#dropdown-tags').toggle();
         return false;
-      })
+      });
+      self.el.on('keyup', '#tag_name', function(){
+        if ($('#tag_name').val() != "") {
+          $("#tag_submit").attr("disabled",false);
+        } else {
+          $("#tag_submit").attr("disabled","disabled");
+        }
+      });
     }
   }
 
