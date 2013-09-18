bcebad76ccd4d2d15368a987b6a1411736d42219
Cxg <xg.chen87@gmail.com>
2013-09-18 11:56:00 +0800

    加入验证格式

--- spec/features/tag_spec.rb
+++ spec/features/tag_spec.rb
@@ -13,18 +13,37 @@ describe "topic section" do
       visit organization_topics_path(@organization)
     end
 
-    context "in the topic list page, click the tagging-group control", :js => true do
+    context "in the topic list page, click the tagging-group control" do
+      before {click_button "tagging-dropdown"}
       it  "should see the organization tags" do
-        click_button "tagging-dropdown"
-        # Need to replace page with specific div
         page.should have_content @organization.tags.last.name
       end
 
       it "should see the newly created tag", :js => true do
-        click_button "tagging-dropdown"
         fill_in "tag_name", :with => "新标签"
         click_button "新建"
         page.should have_content "新标签"
+      end
+
+      it "cannot create same tag twice", :js => true do
+        fill_in "tag_name", :with => "同一个标签"
+        click_button "新建"
+        page.should have_content "同一个标签"
+        fill_in "tag_name", :with => "同一个标签"
+        click_button "新建"
+        page.should have_content "标签名已使用"
+      end
+
+      it "cannot create blank tag", :js => true do
+        fill_in "tag_name", :with => " "
+        click_button "新建"
+        page.should have_content "标签名不能为空"
+      end
+
+      it "cannot create tag with mark other than dash and underscore", :js => true do
+        fill_in "tag_name", :with => "错误%标签"
+        click_button "新建"
+        page.should have_content "名称不合法"
       end
     end
   end
