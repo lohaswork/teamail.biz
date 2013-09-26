301b1adc3a0fef6a53b02ebe2644532244334cbd
Cxg <xg.chen87@gmail.com>
2013-09-27 00:30:22 +0800

    remove unused helper method

--- app/controllers/controller_modules/topic.rb
+++ app/controllers/controller_modules/topic.rb
@@ -1,10 +1,6 @@
 module ControllerModules::Topic
     extend ActiveSupport::Concern
 
-  included do
-    helper_method :order_by_updator
-  end
-
   protected
     def order_by_updator(topics)
       topics.sort_by { |topic| topic.last_active_time }.reverse
