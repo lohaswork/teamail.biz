92a1692d39bdca33cc0f6667d6e9be57b1b28272
Cxg <xg.chen87@gmail.com>
2013-09-28 00:59:47 +0800

    修改初始default_organization_id赋值问题

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
       raise ValidationError.new(organ.errors.full_messages)if !organ.valid?
       user.organizations << organ
       organ.save!
-      user.default_organization = organ
+      user.default_organization_id = organ.id
       user.save!
       user
     end
