e90acd4dcb7535004f8cfc7ae4d58b191d36bb07
Cxg <xg.chen87@gmail.com>
2013-10-11 18:24:17 +0800

    change name and favicon

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/www/teamail_deploy"
 # Repository
 set :application, "LohasWork.com"
 set :scm, :git
-set :repository,  "git@github.com:lohaswork/TeaMail.biz.git"
+set :repository,  "git@github.com:lohaswork/teamail.biz.git"
 set :branch, "master"  # Need changge to master
 
 # Configurations
