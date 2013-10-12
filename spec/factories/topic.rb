2487c25104e1239251176ceb96396212a05931eb
Cxg <xg.chen87@gmail.com>
2013-10-12 10:28:35 +0800

    fix test bug

--- spec/factories/topic.rb
+++ spec/factories/topic.rb
@@ -1,9 +1,6 @@
 FactoryGirl.define do
   factory :topic do
     title
-    after(:create) do |topic|
-      discussion = create(:discussion, discussable: topic)
-    end
   end
 
   sequence :title do |n|
