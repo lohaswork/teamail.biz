f70ea392ca002331222cbdb5cec3a83530d3f99d
Cxg <xg.chen87@gmail.com>
2013-10-31 17:53:48 +0800

    change test@lohaswork.com to test@mail.teamail.biz for webfaction spam issue

--- config/config.rb
+++ config/config.rb
@@ -1,7 +1,7 @@
 {
   default:{
     mailgun_api_key: "key-6di1x56ytcjjlsfb7vw9gbs-ht1x1z-2",
-    receive_test_email:"test@lohaswork.com",
+    receive_test_email:"test@mail.teamail.biz",
     default_system_email: "notifications@mail.teamail.biz",
     email: true
     },
