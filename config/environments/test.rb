2482d6d4b49a61bdfa94d3cd39fadb7ad4890e4f
Cxg <xg.chen87@gmail.com>
2013-11-01 11:27:36 +0800

    modify navbar and some redirect behaviour

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,7 +12,7 @@ LohasWorkCom::Application.configure do
 
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=3600"
+  config.static_cache_control = "public, max-age=300"
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
