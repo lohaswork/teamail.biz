ac039c30db5cc70e5956a143217d7bfc2c987d36
Cxg <xg.chen87@gmail.com>
2013-11-02 15:24:14 +0800

    minor change in sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,8 +4,6 @@ Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq' }
 end
 
-unless Rails.env.production?
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq'  }
-  end
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDIS_URL"], namespace: 'sidekiq'  }
 end
