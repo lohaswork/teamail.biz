ab0ccf1f6609db349b13d8eb90916e9f1327463e
Cxg <xg.chen87@gmail.com>
2013-11-03 11:43:13 +0800

    fix tag spec

--- spec/features/tag_spec.rb
+++ spec/features/tag_spec.rb
@@ -198,14 +198,14 @@ describe "topic section" do
         page.should_not have_css("div.topic-row")
       end
 
-      it "click tag and it turns active and others become inactive" do
+      it "click two tags and they both turn active" do
         first_link = all(:css, "div#tag-filters a").first
         last_link = all(:css, "div#tag-filters a").last
         first_link.click
         all(:css, "div#tag-filters").first.should have_css("li.active-tag")
         last_link.click
         all(:css, "div#tag-filters").last.should have_css("li.active-tag")
-        all(:css, "div#tag-filters").first.should_not have_css("li.active-tag")
+        all(:css, "div#tag-filters").first.should have_css("li.active-tag")
       end
     end
 
@@ -236,14 +236,14 @@ describe "topic section" do
         page.should_not have_css("div.topic-row")
       end
 
-      it "click tag and it turns active and others become inactive" do
+      it "click two tags and they both turn active" do
         first_link = all(:css, "div#tag-filters a").first
         last_link = all(:css, "div#tag-filters a").last
         first_link.click
         all(:css, "div#tag-filters").first.should have_css("li.active-tag")
         last_link.click
         all(:css, "div#tag-filters").last.should have_css("li.active-tag")
-        all(:css, "div#tag-filters").first.should_not have_css("li.active-tag")
+        all(:css, "div#tag-filters").first.should have_css("li.active-tag")
       end
     end
 
