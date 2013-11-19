b71e16424299d320778afc2ff8c6601979d90fda
Cxg <xg.chen87@gmail.com>
2013-11-19 19:42:35 +0800

    render 406 code when email create success but notification-emails fail to deliver

--- app/controllers/email_receivers_controller.rb
+++ app/controllers/email_receivers_controller.rb
@@ -1,10 +1,17 @@
 # encoding: utf-8
 class EmailReceiversController < ApplicationController
+  rescue_from Exceptions::PostEmailReceiveError, with: :render_406
+
   def email
     #should set the route for mailgun manully for development
     email = EmailEngine::EmailReceiver::Email.new request.POST
     #create the topic or discussion from email
     email.create_from_email
     render :nothing => true
+  end
+
+  private
+  def render_406
+    render :nothing => true, :status => 406
   end
 end
