780686cac7d8134a684ae0f8b0b8fc20258ddfa5
Cxg <xg.chen87@gmail.com>
2013-12-12 10:45:48 +0800

    remove welcome page, add help page and make it the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 LohasWorkCom::Application.routes.draw do
-  root 'welcome#index'
+  root 'home#index'
 
   controller :home do
     get 'home' => :index, as: :home
     get 'about-us' => :about_us, as: :about_us
     get 'faq' => :faq
     get 'agreement' => :agreement
+    get 'help' => :help
   end
 
   controller :users do
