555aaf1b909be8f0302b4bba147a0c343ca19207
charleschu <cuxang@gmail.com>
2014-03-09 15:14:59 +0800

    tags filter works

--- app/api/api.rb
+++ app/api/api.rb
@@ -17,18 +17,18 @@ module API
       # Todo: paginate & tag filter
       desc "Return current user personal topics."
       params do
-        optional :tags do
-          requires :id, type: Integer
-        end
+        optional :tags, :type => Array, :desc => "Tag IDs for filter"
         requires :mailbox_type, :type => String, :desc => "MailBox type"
         requires :per_page, :type => Integer, :desc => "Page size for pagination"
         requires :page, :type => Integer, :desc => "Page number for pagination"
       end
       get "/personal", jbuilder: 'topics/personal.jbuilder' do
         guard!
-        topics = paginate current_user.personal_topics(params[:mailbox_type])
+        topics = current_user.personal_topics(params[:mailbox_type])
         if params[:tags].present?
-          @topics = topics.select { |topic| (topic.tags.map(&:id) <=> params[:tags]) != -1 }
+          @topics = Kaminari.paginate_array(topics.reject { |topic| !topic.has_tags?(params[:tags]) }).page(params[:page])
+        else
+          @topics = paginate topics
         end
       end
     end
