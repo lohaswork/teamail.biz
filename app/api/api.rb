218835425114624f6387bb41f1040edc9f397fd2
charleschu <cuxang@gmail.com>
2014-03-09 16:22:37 +0800

    1.personal topic has no tags filter 2.organization topic works

--- app/api/api.rb
+++ app/api/api.rb
@@ -11,20 +11,37 @@ module API
         @current_user ||= OauthAccessToken.find_by(:token, params[:access_token]).user
       end
 
+      def current_organization
+        begin
+          @current_organization ||= ::Organization.for_user(@current_user).find(@current_user.default_organization_id)
+        rescue
+          nil
+        end
+      end
     end
 
     resources :topics do
-      # Todo: paginate & tag filter
       desc "Return current user personal topics."
+
       params do
-        optional :tags, :type => Array, :desc => "Tag IDs for filter"
-        requires :mailbox_type, :type => String, :desc => "MailBox type"
         requires :per_page, :type => Integer, :desc => "Page size for pagination"
         requires :page, :type => Integer, :desc => "Page number for pagination"
       end
+
       get "/personal", jbuilder: 'topics/personal.jbuilder' do
         guard!
-        topics = current_user.personal_topics(params[:mailbox_type])
+        params do
+          requires :mailbox_type, :type => String, :desc => "MailBox type"
+        end
+        @topics = paginate current_user.personal_topics(params[:mailbox_type])
+      end
+
+      get "/organization", jbuilder: 'topics/organization.jbuilder' do
+        guard!
+        params do
+          optional :tags, :type => Array, :desc => "Tag IDs for filter"
+        end
+        topics = current_organization.topics.order_by_update
         if params[:tags].present?
           @topics = Kaminari.paginate_array(topics.reject { |topic| !topic.has_tags?(params[:tags]) }).page(params[:page])
         else
