298ca565d499995609ce963d636b58cb1909551d
charleschu <cuxang@gmail.com>
2014-04-13 14:53:55 +0800

    add organization topic spec

--- spec/requests/topic_spec.rb
+++ spec/requests/topic_spec.rb
@@ -8,12 +8,24 @@ describe "Topics request" do
     @access_token = JSON.parse(response.body)["access_token"]
   end
 
-  it "get token" do
+  it "get personal topics" do
     get "/api/v1/topics/personal?access_token=#{@access_token}&mailbox_type=all&page=1&per_page=2"
     body = JSON.parse(response.body)
     body["topics"].size.should == 2
     body["total_count"].should == 10
     topic = body["topics"][1]
+    %w(id creator title update_at has_attachments read_status tags).each do |key|
+      topic.should have_key key
+    end
+  end
+
+  it "get organization topics" do
+    get "/api/v1/topics/organization?access_token=#{@access_token}&page=1&per_page=2"
+    body = JSON.parse(response.body)
+    body["topics"].size.should == 2
+    body["total_count"].should == 10
+    topic = body["topics"][1]
+    debugger
     %w(id creator title update_at has_attachments read_status tags).each do |key|
       topic.should have_key key
     end
