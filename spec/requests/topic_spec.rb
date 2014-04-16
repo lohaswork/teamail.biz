79f702c0c76ad276bf2d08f31465ca5dafa223d3
charleschu <cuxang@gmail.com>
2014-04-16 18:54:05 +0800

    use let

--- spec/requests/topic_spec.rb
+++ spec/requests/topic_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe "Topics request" do
   let(:user) { create(:normal_user) }
-  before do
+  let :access_token do
     url = "/api/v1/oauth/token?" + {:email => user.email, :password => user.password, :grant_type => 'password'}.to_query
     post url
-    @access_token = JSON.parse(response.body)["access_token"]
+    JSON.parse(response.body)["access_token"]
   end
 
   describe "personal topics" do
     it "should get personal topics" do
-      get "/api/v1/topics/personal?access_token=#{@access_token}&mailbox_type=all&page=1&per_page=2"
+      get "/api/v1/topics/personal?access_token=#{access_token}&mailbox_type=all&page=1&per_page=2"
       body = JSON.parse(response.body)
       body["topics"].size.should == 2
       body["total_count"].should == 10
@@ -21,42 +21,42 @@ describe "Topics request" do
     end
 
     it "should get the error message when missing params page" do
-      get "/api/v1/topics/personal?access_token=#{@access_token}&mailbox_type=all&per_page=2"
+      get "/api/v1/topics/personal?access_token=#{access_token}&mailbox_type=all&per_page=2"
       body = JSON.parse(response.body)
       body.should have_value "Bad Request"
       body.should have_value "page is missing"
     end
 
     it "should get the error message when pass invalid params page" do
-      get "/api/v1/topics/personal?access_token=#{@access_token}&mailbox_type=all&per_page=2&page=not_number"
+      get "/api/v1/topics/personal?access_token=#{access_token}&mailbox_type=all&per_page=2&page=not_number"
       body = JSON.parse(response.body)
       body.should have_value "Bad Request"
       body.should have_value "page is invalid"
     end
 
     it "should get the error message when missing params per_page" do
-      get "/api/v1/topics/personal?access_token=#{@access_token}&mailbox_type=all&page=2"
+      get "/api/v1/topics/personal?access_token=#{access_token}&mailbox_type=all&page=2"
       body = JSON.parse(response.body)
       body.should have_value "Bad Request"
       body.should have_value "per_page is missing"
     end
 
     it "should get the error message when pass invalid params per_page" do
-      get "/api/v1/topics/personal?access_token=#{@access_token}&mailbox_type=all&page=2&per_page=not_number"
+      get "/api/v1/topics/personal?access_token=#{access_token}&mailbox_type=all&page=2&per_page=not_number"
       body = JSON.parse(response.body)
       body.should have_value "Bad Request"
       body.should have_value "per_page is invalid"
     end
 
     it "should get the error message when missing params mailbox_type" do
-      get "/api/v1/topics/personal?access_token=#{@access_token}&page=1&per_page=2"
+      get "/api/v1/topics/personal?access_token=#{access_token}&page=1&per_page=2"
       body = JSON.parse(response.body)
       body.should have_value "Bad Request"
       body.should have_value "mailbox_type is missing"
     end
 
     it "should get the error message when pass invalid params mailbox_type" do
-      get "/api/v1/topics/personal?access_token=#{@access_token}&mailbox_type=invalid&page=1&per_page=2"
+      get "/api/v1/topics/personal?access_token=#{access_token}&mailbox_type=invalid&page=1&per_page=2"
       body = JSON.parse(response.body)
       body.should have_value "Bad Request"
       body.should have_value "mailbox_type does not have a valid value"
@@ -66,7 +66,7 @@ describe "Topics request" do
 
   describe "organization topics" do
     it "should get organization topics" do
-      get "/api/v1/topics/organization?access_token=#{@access_token}&page=1&per_page=2"
+      get "/api/v1/topics/organization?access_token=#{access_token}&page=1&per_page=2"
       body = JSON.parse(response.body)
       body["topics"].size.should == 2
       body["total_count"].should == 10
